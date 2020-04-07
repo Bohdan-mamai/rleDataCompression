af41b22ac1e5d21cb27029a964a534b1c8129d07
Bohdan_Mamai <b909mamay@gmail.com>
2020-04-07 20:59:21 +0300

    First commit

---
+++ src/CoderFunctionality.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CoderFunctionality {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        String regex = "[0-9]";
+
+        String slash = ("\\\\");
+
+        String str = "";
+
+        int count = 1;
+
+        char[] masChar = s.toCharArray();
+        for (int i = 0; i < masChar.length - 1; i++) {
+            String number = String.valueOf(masChar[i]);
+            if (number.matches(regex) || number.matches(slash)) {
+                str = str + "\\" + masChar[i];
+            } else {
+                if (masChar[i] == masChar[i + 1] && count < 9) {
+                    count++;
+                } else {
+                    if (count == 1) {
+                        str = str + count + masChar[i];
+                    } else {
+                        str = str + count + masChar[i];
+                        count = 1;
+                    }
+                }
+            }
+        }
+        if (count == 1) {
+            str = str + "\\" + masChar[masChar.length - 1];
+        } else {
+            str = str + count + masChar[masChar.length - 1];
+        }
+
+        System.out.println(str);
+    }
+}
